88e424d0a9577bcd93fc3ee5536c53c7826e764c
Alex Benoit <alexander.benoit@outlook.com>
2016-10-22 21:24:14 +0100

    added strava gem

--- app/controllers/swims_controller.rb
+++ app/controllers/swims_controller.rb
@@ -1,6 +1,6 @@
 class SwimsController < ApplicationController
   def create
-    Swim.create(user: current_user, challenge: Challenge.find(params[:event_id]))
+    Swim.create(user: current_user, challenge: Challenge.find(params[:challenge_id]))
     redirect_to challenges_path
   end
 end
